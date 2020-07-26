4677511cc9c616c9c1de5b6b7a949757d65fa4e7
autimio <autimio.dev@gmail.com>
2020-07-26 20:12:47 -0300

    add images locally

---
+++ src/utils/Flag.js
@@ -0,0 +1,11 @@
+import africadosul from '../assets/images/africadosul.jpg';
+import brasil from '../assets/images/brasil.jpg';
+import colombia from '../assets/images/colombia.jpg';
+import estadosunidos from '../assets/images/estadosunidos.jpg';
+
+export default {
+  africadosul,
+  brasil,
+  colombia,
+  estadosunidos,
+};
