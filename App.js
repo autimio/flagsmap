fdf2563f0becad25b5aeaf74490aa64a56b22fe9
autimio <autimio.dev@gmail.com>
2020-07-26 01:12:13 -0300

    initial structure for plotting the flags and installing the underscore

--- App.js
+++ App.js
@@ -1,46 +1,98 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
-import {StyleSheet, View} from 'react-native';
+import _ from 'underscore';
 
 MapboxGL.setAccessToken(
   'pk.eyJ1IjoiYXV0aW1pbyIsImEiOiJja2F0MnZtdjAwZGNqMnhucm9zZDRib3diIn0.BP7QhaCLplGE_Y09tF7qAQ',
 );
 
-MapboxGL.setConnected(true);
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  container: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: 'tomato',
-  },
-  map: {
-    flex: 1,
-  },
-});
-
 function App() {
+  const [images, setImages] = useState({});
+  const [shape, setShape] = useState({features: [], type: 'FeatureCollection'});
+
+  useEffect(() => {
+    function onMountFeatures() {
+      let imagesIcon = {};
+      let features = [...shape.features];
+      let propertiesIds = _.pluck(features, 'properties');
+      let featuresIds = _.pluck(propertiesIds, 'id');
+
+      function addShape(items) {
+        items.map((item) => {
+          if (!_.contains(featuresIds, item.id)) {
+            imagesIcon[item.id] = item.image;
+            features.push({
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: item.coordinates,
+              },
+              properties: {
+                id: item.id,
+                icon: item.id,
+                showPin: true,
+              },
+            });
+          }
+        });
+
+        setShape({type: 'FeatureCollection', features});
+        setImages(imagesIcon);
+      }
+
+      addShape([
+        {
+          id: 'islandia',
+          coordinates: [-101.3818359, 39.7747695],
+          image:
+            'https://www.sogeografia.com.br/figuras/bandeiras/america/mini/estadosunidos.jpg',
+        },
+        {
+          id: 'brasil',
+          coordinates: [-49.286041, -16.665136],
+          image:
+            'https://www.sogeografia.com.br/figuras/bandeiras/america/mini/brasil.jpg',
+        },
+        {
+          id: 'colombia',
+          coordinates: [-73.0810547, 4914894],
+          image:
+            'https://www.sogeografia.com.br/figuras/bandeiras/america/mini/colombia.jpg',
+        },
+        {
+          id: 'africa do sul',
+          coordinates: [24.2578125, -30.9022247],
+          image:
+            'https://www.sogeografia.com.br/figuras/bandeiras/africa/mini/africadosul.jpg',
+        },
+      ]);
+    }
+    onMountFeatures();
+  }, []);
+
   return (
-    <View style={styles.page}>
-      <View style={styles.container}>
-        <MapboxGL.MapView style={styles.map} />
-      </View>
-    </View>
+    <MapboxGL.MapView style={{flex: 1}} styleURL={MapboxGL.StyleURL.Dark}>
+      <MapboxGL.Camera centerCoordinate={[-10, -5]} zoomLevel={0.5} />
+      <MapboxGL.Images images={images} />
+      <MapboxGL.ShapeSource
+        id="shapeSourceDefault"
+        clusterRadius={10}
+        clusterMaxZoomLevel={8}
+        cluster={true}
+        shape={shape}>
+        <MapboxGL.SymbolLayer
+          id="{id}"
+          minZoomLevel={0}
+          style={{
+            iconImage: ['get', 'icon'],
+            iconOptional: true,
+            iconAllowOverlap: true,
+            iconSize: 0.5,
+          }}
+        />
+      </MapboxGL.ShapeSource>
+    </MapboxGL.MapView>
   );
 }
 
